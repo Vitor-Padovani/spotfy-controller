d170a939a1f9bf3935494b3d49ed4addbbf07f4a
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-03-09 08:47:41 -0300

    room_view_setup

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
+# secondary url redirector
 urlpatterns = [
-    path('', main)
+    path('room', RoomView.as_view()),
 ]
-
